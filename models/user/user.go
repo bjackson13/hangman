7778884039479e0e79ae4c3e56fc7fc8ade2d025
BrenBren <btj9560@rit.edu>
2020-10-11 12:57:28 -0400

    Untested user repo functions

--- models/user/user.go
+++ models/user/user.go
@@ -6,4 +6,20 @@ type User struct {
 	Username string
 	IP string
 	UserAgent string
+	password string
+}
+
+/*
+	Password getters and setters are used to allow the password field 
+		to remain unexported so it is not manipulated when not needed
+*/
+
+/*SetPassword - set the user password*/
+func (user *User) SetPassword(password string) {
+	user.password = password
+}
+
+/*GetPassword - get the user password*/
+func (user *User) GetPassword() string {
+	return user.password
 }
--- models/user/userrepo.go
+++ models/user/userrepo.go
@@ -22,7 +22,7 @@ func (repo *Repo) GetUser(username string, password string) (User, error) {
 	conn := repo.db.Connection
 	userStmt, err := conn.Prepare("SELECT UserId, Username, IP, UserAgent FROM User WHERE username = ? AND password = ?")
 	if err != nil {
-		panic(err.Error())
+		return nul, err
 	}
 	defer userStmt.Close()
 	
@@ -30,4 +30,75 @@ func (repo *Repo) GetUser(username string, password string) (User, error) {
 	err = userStmt.QueryRow(username, password).Scan(&user.UserID, &user.Username, &user.IP, &user.UserAgent)
 
 	return user, err
+}
+
+/*AddUser - Add a user to database*/
+func (repo *Repo) AddUser(user User) (User, error) {
+
+	conn := repo.db.Connection
+	userStmt, err := conn.Prepare("INSERT INTO User(Username, Password, IP, UserAgent) VALUES (?,?,?,?)")
+	if err != nil {
+		return err
+	}
+	defer userStmt.Close()
+
+	res, err := userStmt.Exec(user.Username, user.GetPassword(), user.IP, user.UserAgent)
+	if err == nil {
+		user.UserID = res.LastInsertId()
+	}
+	return user, err
+}
+
+/*UpdateUser - update an antire user. Returns rows affected or an error*/
+func (repo *Repo) UpdateUser(user User) (int, error) {
+	conn := repo.db.Connection
+	userStmt, err := conn.Prepare("UPDATE User SET Username = ?, Password = ?, IP = ?, UserAgent = ? WHERE UserId = ?")
+	if err != nil {
+		return 0,err
+	}
+	defer userStmt.Close()
+
+	res, err := userStmt.Exec(user.Username, user.GetPassword(), user.IP, user.UserAgent, user.UserID)
+	if err != nil {
+		return 0,err
+	}
+
+	rows, err := res.RowsAffected()
+	return rows, err
+}
+
+/*UpdateUserIdentifiers - update just the UserAgent and IP fields of a user*/
+func (repo *Repo) UpdateUserIdentifiers(user User) (User, error) {
+	conn := repo.db.Connection
+	userStmt, err := conn.Prepare("UPDATE User SET IP = ?, UserAgent = ? WHERE UserId = ?")
+	if err != nil {
+		return 0,err
+	}
+	defer userStmt.Close()
+
+	res, err := userStmt.Exec(user.IP, user.UserAgent, user.UserID)
+	if err != nil {
+		return 0,err
+	}
+	
+	rows, err := res.RowsAffected()
+	return rows, err
+}
+
+/*DeleteUser - remove a user from the DB*/
+func (repo *Repo) DeleteUser(user User) error {
+	conn := repo.db.Connection
+	userStmt, err := conn.Prepare("DELETE FROM User WHERE UserId = ?")
+	if err != nil {
+		return 0,err
+	}
+	defer userStmt.Close()
+
+	res, err := userStmt.Exec(user.UserID)
+	if err != nil {
+		return 0,err
+	}
+	
+	rows, err := res.RowsAffected()
+	return rows, err
 }
