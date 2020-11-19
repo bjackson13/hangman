77569515ff398637400e9858b2dd8cde4b457724
BrenBren <btj9560@rit.edu>
2020-11-18 21:15:24 -0500

    Removed unneded cgi stuff

--- main.go
+++ main.go
@@ -4,9 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/bjackson13/hangman/controllers"
 	"github.com/bjackson13/hangman/services/config"
-	"net/http/cgi" //for when I try to deploy on solcace
 	"log"
-	"os"
 )
 
 func main() {
@@ -15,8 +13,6 @@ func main() {
 		log.Fatal("Failed to load env variables")
 		return
 	}
-	
-	isCGI := os.Getenv("CGI_USE")
 
 	/*
 		Create a Gin router and attach routes to it through controller methods.
@@ -30,10 +26,5 @@ func main() {
 	controllers.RegisterGameRoutes(router)
 	controllers.RegisterChatRoutes(router)
 
-	if isCGI == "true" {
-		//For use with Common gateway interface (I am going to attempt to deploy on solace)
-		log.Fatal(cgi.Serve(router))
-	} else {
-		router.Run()
-	}
+	router.Run()
 }
